97cbf369111bb3fa04db1588a52063333f3640ba
m1k53r <mikobw369@protonmail.com>
2025-04-02 21:44:14 +0200

    init

---
+++ src/editor/canvas-controller.tsx
@@ -0,0 +1,19 @@
+import { Canvas, ThreeElements, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import TorusController from './torus-controller'
+
+function CanvasController() {
+  return (
+    <>
+      <Canvas camera={{position: [0, 0, 30]}}>
+        <ambientLight />
+        <directionalLight position={[10, 10, 10]} />
+        <TorusController position={[-5, 0, 0]} />
+        <TorusController position={[5, 0, 0]} />
+        <OrbitControls enableDamping={false} />
+      </Canvas>
+    </>
+  )
+}
+
+export default CanvasController
---
+++ src/editor/torus-controller.tsx
@@ -0,0 +1,30 @@
+import { useRef, useState } from 'react'
+import * as THREE from 'three'
+import { ThreeElements, useFrame } from '@react-three/fiber'
+
+function TorusController(props: ThreeElements['mesh']) {
+  const ref = useRef<THREE.Mesh>(null!)
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false);
+
+  useFrame((_, delta) => {
+    ref.current.rotation.z += delta
+    ref.current.rotation.y += delta
+    ref.current.rotation.x += delta
+  })
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(_) => click(!clicked)}
+      onPointerOver={(_) => hover(true)}
+      onPointerOut={(_) => hover(false)}>
+      <torusGeometry args={[3, 1, 16, 100]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+export default TorusController
