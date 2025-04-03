7ce312eca05bdb4f1788daafa81b619c2b82a997
Mikołaj Szustakowski <55512451+m1k53r@users.noreply.github.com>
2025-04-03 12:22:50 +0200

    feature: add prettier (#13)
    * init
    
    * run formatter on project files

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,12 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -19,10 +20,12 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'prettier/prettier': 'error',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
   },
+  prettier,
 )
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css'
-import CanvasController from './editor/canvas-controller'
+import './App.css';
+import CanvasController from './editor/canvas-controller';
 
 function App() {
   return (
     <>
       <CanvasController />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
