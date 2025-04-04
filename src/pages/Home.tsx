c32e7802794915ddd2b9107c92053c3a749c8d8a
Szymon Mastalerz <33292538+simon04PL@users.noreply.github.com>
2025-04-04 08:05:42 +0200

    add basic homescreen (#20)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,18 @@
-function Home () {
+import { Link } from 'react-router-dom';
+
+function Home() {
   return (
-    <h1>About Page</h1>
+    <section className="l-section l-section--home">
+      <h1>Welcome to CADR</h1>
+      <p>Your 3D modeling and rendering tool.</p>
+      <div className="home__btn--section" >
+        <Link to="/editor" className="home__link">
+          <button className="home__btn">
+            Go to Editor
+          </button>
+        </Link>
+      </div>
+    </section>
   );
 }
 
